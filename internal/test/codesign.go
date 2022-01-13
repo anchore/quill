5683b2e6a2d45ed3b223b32bce48c995bc8ce837
Alex Goodman <alex.goodman@anchore.com>
2022-01-13 13:27:40 -0500

    add failing test for signing
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- internal/test/codesign.go
+++ internal/test/codesign.go
@@ -1,7 +1,62 @@
 package test
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"testing"
+)
 
-func AssertBinarySigned(t *testing.T) {
+func AssertBinarySigned(t *testing.T, path string) {
+	// TODO: assert specific traits from debug output
+	runCodesignVerify(t, path)
+}
 
+func runCodesignVerify(t testing.TB, path string) string {
+	cmd := exec.Command("codesign", "-d", "--verbose=6", "--verify", path)
+	stdout, stderr := runCommand(cmd, nil)
+	if cmd.ProcessState.ExitCode() != 0 {
+		t.Log("STDOUT", stdout)
+		t.Log("STDERR", stderr)
+		t.Log("codesign verify failed")
+		t.Fail()
+	}
+	return stdout
+}
+
+func runCodesignShow(t testing.TB, path string) string {
+	cmd := exec.Command("codesign", "-d", "--verbose=6", path)
+	stdout, stderr := runCommand(cmd, nil)
+	if cmd.ProcessState.ExitCode() != 0 {
+		t.Log("STDOUT", stdout)
+		t.Log("STDERR", stderr)
+		t.Log("codesign show failed")
+		t.Fail()
+	}
+	return stdout
+}
+
+func runCommand(cmd *exec.Cmd, env map[string]string) (string, string) {
+	if env != nil {
+		cmd.Env = append(os.Environ(), envMapToSlice(env)...)
+	}
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	// ignore errors since this may be what the test expects
+	cmd.Run()
+
+	return stdout.String(), stderr.String()
+}
+
+func envMapToSlice(env map[string]string) (envList []string) {
+	for key, val := range env {
+		if key == "" {
+			continue
+		}
+		envList = append(envList, fmt.Sprintf("%s=%s", key, val))
+	}
+	return
 }
--- pkg/sign/sign_test.go
+++ pkg/sign/sign_test.go
@@ -1,23 +1,40 @@
 package sign
 
-//func TestSign(t *testing.T) {
-//	type args struct {
-//		id          string
-//		path        string
-//		keyFile     string
-//		keyPassword string
-//		certFile    string
-//	}
-//	tests := []struct {
-//		name    string
-//		args    args
-//		wantErr assert.ErrorAssertionFunc
-//	}{
-//		{},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			tt.wantErr(t, Sign(tt.args.id, tt.args.path, tt.args.keyFile, tt.args.keyPassword, tt.args.certFile), fmt.Sprintf("Sign(%v, %v, %v, %v, %v)", tt.args.id, tt.args.path, tt.args.keyFile, tt.args.keyPassword, tt.args.certFile))
-//		})
-//	}
-//}
+import (
+	"testing"
+
+	"github.com/anchore/quill/internal/test"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSign(t *testing.T) {
+	test.Make(t, "fixture-hello")
+
+	type args struct {
+		id          string
+		path        string
+		keyFile     string
+		keyPassword string
+		certFile    string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "sign the hello binary",
+			args: args{
+				id:       "my-id",
+				path:     test.AssetCopy(t, "hello"),
+				keyFile:  test.Asset(t, "hello-key.pem"),
+				certFile: test.Asset(t, "hello-cert.pem"),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.NoError(t, Sign(tt.args.id, tt.args.path, tt.args.keyFile, tt.args.keyPassword, tt.args.certFile))
+			test.AssertBinarySigned(t, tt.args.path)
+		})
+	}
+}
