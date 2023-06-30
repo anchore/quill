10fd978c9997545839748762096f0ad0950fa900
Alex Goodman <wagoodman@users.noreply.github.com>
2023-06-30 12:37:29 -0400

    allow for lib consumers to set a redactor
    Signed-off-by: Alex Goodman <wagoodman@users.noreply.github.com>

--- quill/lib.go
+++ quill/lib.go
@@ -1,11 +1,13 @@
 package quill
 
 import (
+	"github.com/anchore/go-logger/adapter/redact"
 	"github.com/wagoodman/go-partybus"
 
 	"github.com/anchore/go-logger"
 	"github.com/anchore/quill/internal/bus"
 	"github.com/anchore/quill/internal/log"
+	intRedact "github.com/anchore/quill/internal/redact"
 )
 
 // SetLogger sets the logger object used for all logging calls.
@@ -16,4 +18,8 @@ func SetLogger(logger logger.Logger) {
 // SetBus sets the event bus for all library bus publish events onto (in-library subscriptions are not allowed).
 func SetBus(b *partybus.Bus) {
 	bus.Set(b)
+}
+
+func SetRedactStore(store redact.Store) {
+	intRedact.Set(store)
 }
