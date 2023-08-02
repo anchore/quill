653dd6996a048881dd0a91306c9fad34a93d9bf8
Keith Zantow <kzantow@gmail.com>
2023-08-02 10:25:54 -0400

    chore: update to latest clio (#98)
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- cmd/quill/main.go
+++ cmd/quill/main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"context"
-	"os"
-	"os/signal"
-	"strings"
-
-	"github.com/gookit/color"
-
 	"github.com/anchore/clio"
 	"github.com/anchore/quill/cmd/quill/cli"
 	"github.com/anchore/quill/internal"
-	"github.com/anchore/quill/internal/log"
 )
 
 const valueNotProvided = "[not provided]"
@@ -23,7 +15,7 @@ var gitDescription = valueNotProvided
 var buildDate = valueNotProvided
 
 func main() {
-	cmd := cli.New(
+	app := cli.New(
 		clio.Identification{
 			Name:           internal.ApplicationName,
 			Version:        version,
@@ -33,44 +25,5 @@ func main() {
 		},
 	)
 
-	// drive application control from a single context which can be cancelled (notifying the event loop to stop)
-	ctx, cancel := context.WithCancel(context.Background())
-	cmd.SetContext(ctx)
-
-	// note: it is important to always do signal handling from the main package. In this way if quill is used
-	// as a lib a refactor would not need to be done (since anything from the main package cannot be imported this
-	// nicely enforces this constraint)
-	signals := make(chan os.Signal, 10) // Note: A buffered channel is recommended for this; see https://golang.org/pkg/os/signal/#Notify
-	signal.Notify(signals, os.Interrupt)
-
-	var exitCode int
-
-	defer func() {
-		if exitCode != 0 {
-			os.Exit(exitCode)
-		}
-	}()
-
-	defer func() {
-		signal.Stop(signals)
-		cancel()
-	}()
-
-	go func() {
-		select {
-		case <-signals: // first signal, cancel context
-			log.Trace("signal interrupt, stop requested")
-			cancel()
-		case <-ctx.Done():
-		}
-		<-signals // second signal, hard exit
-		log.Trace("signal interrupt, killing")
-		os.Exit(1)
-	}()
-
-	if err := cmd.Execute(); err != nil {
-		// report an issue on stdout
-		color.Red.Println(strings.TrimSpace(err.Error()))
-		exitCode = 1
-	}
+	app.Run()
 }
